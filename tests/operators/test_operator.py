7c493df3ccbec7c5fbf7fe955135be89eb8b3e27
Andrew Epps <andrew.epps@tes.com>
2021-12-08 15:52:25 +0000

    Add test to check operator throws an error if given a non-existent exhange name.
    data/#1796

--- tests/operators/test_operator.py
+++ tests/operators/test_operator.py
@@ -1,3 +1,4 @@
+import pika
 import pytest
 
 from rabbitmq_provider.operators.rabbitmq import RabbitMQOperator
@@ -16,3 +17,21 @@ def test_operator(monkeypatch):
         message="Hello world",
     )
     operator.execute(context={})
+
+
+def test_bad_exchange_name(monkeypatch):
+    monkeypatch.setenv(
+        "AIRFLOW_CONN_CONN_RABBITMQ", "amqp://guest:guest@localhost:5672"
+    )
+
+    with pytest.raises(pika.exceptions.ChannelClosedByBroker,
+                       match="""\(404, \"NOT_FOUND - no exchange \'does_not_exist\' in vhost""",):
+
+        operator = RabbitMQOperator(
+            task_id="run_id",
+            rabbitmq_conn_id="conn_rabbitmq",
+            exchange="does_not_exist",
+            routing_key="test",
+            message="Hello world",
+        )
+        operator.execute(context={})
